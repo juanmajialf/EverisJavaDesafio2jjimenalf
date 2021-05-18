7e4c0ad640c3b9ac675d645d602b186823d5f908
juanmajialf <juanma.jimenez.alfaro@gmail.com>
2021-05-18 18:06:55 +0200

    version 1

---
+++ src/fpdualeveris/FPDual.java
@@ -0,0 +1,19 @@
+package fpdualeveris;
+
+import operators.Pinball;
+
+public class FPDual {
+
+	public static void main(String[] args) {
+		operatorChallenge();
+
+	}
+	
+	private static void operatorChallenge() {
+	
+		Pinball p = new Pinball();
+		p.launchBall();
+	}
+
+}
+
---
+++ src/operators/Pinball.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class Pinball {
+ 
+	public Pinball() {
+		
+	}
+	
+	public void launchBall() {
+		int ball = (int) Math.random()*99+1;
+		
+	}
+}
